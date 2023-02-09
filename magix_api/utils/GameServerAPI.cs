b35838bac625305d216d25d5efa0210781219b44
Karlipouette <karl-marchand@hotmail.com>
2023-02-09 12:04:49 -0500

    Game Server Api calls function

---
+++ magix_api/utils/GameServerAPI.cs
@@ -0,0 +1,38 @@
+using System.Text.Json;
+
+namespace magix_api.utils
+{
+    public static class GameServerAPI
+    {
+        public static async Task<T?> CallApi<T>(string service, Dictionary<string, string>? data = null)
+        {
+            string apiURL = "https://magix.apps-de-cours.com/api/" + service;
+
+            var client = new HttpClient();
+
+            HttpResponseMessage response;
+
+            if (data != null)
+            {
+                var body = new FormUrlEncodedContent(data);
+                response = await client.PostAsync(apiURL, body);
+            }
+            else
+            {
+                response = await client.GetAsync(apiURL);
+            }
+
+            response.EnsureSuccessStatusCode();
+            string result = await response.Content.ReadAsStringAsync();
+
+            if (result.Contains("<br"))
+            {
+                Console.WriteLine(result);
+            }
+
+            return JsonSerializer.Deserialize<T>(result);
+        }
+    }
+
+
+}
