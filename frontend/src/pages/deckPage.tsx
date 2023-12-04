e375f2629a325b6f56c8fe7b6b01aaa09cb0e674
Karlipouette <karl-marchand@hotmail.com>
2023-12-04 09:02:20 -0500

    Started working on deck page

--- frontend/src/pages/deckPage.tsx
+++ frontend/src/pages/deckPage.tsx
@@ -1,7 +1,134 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import RequestHandler from "../utils/requestHandler";
+import AvailableDeckOptions from "../types/AvailableDeckOptions";
+import { Link } from "react-router-dom";
+import { Col, Row } from "react-bootstrap";
+import { Tabs, Tab } from "../components/Tabs/Tabs";
+import Faction from "../types/Faction";
+import Hero from "../types/Hero";
+import Talent from "../types/Talent";
+import Card from "../types/Card";
+import Deck, { DeckBuilder } from "../types/Deck";
+import Avatar from "../components/Avatar/Avatar";
 
 const DeckPage: React.FC = () => {
-	return <div>Deck Page</div>;
+	const [cardList, setCardList] = useState<Card[]>();
+	const [heroList, setHeroList] = useState<Hero[]>();
+	const [talentList, setTalentList] = useState<Talent[]>();
+	const [factionList, setFactionList] = useState<Faction[]>();
+	const [deckList, setDeckList] = useState<Deck[]>();
+	const [currentDeck, setCurrentDeck] = useState<DeckBuilder>(new DeckBuilder());
+
+	useEffect(() => {
+		// Show place holders
+		// Open deck management modal
+		RequestHandler.get<Deck[]>("deck/all").then((response) => {
+			if (response.success && response.data) {
+				setDeckList(response.data);
+			}
+		});
+
+		RequestHandler.get<AvailableDeckOptions>("deck/options/all").then((response) => {
+			if (response.success && response.data) {
+				setCardList(response.data.cards);
+				setHeroList(response.data.heroes);
+				setTalentList(response.data.talents);
+				setFactionList(response.data.factions);
+			}
+			// Remove place holders
+		});
+	}, []);
+
+	const handleSave = (button: EventTarget) => {
+		// Show spinner and disable buttons
+		currentDeck.save().then((response) => {
+			// Remove spinner and enable buttons
+			if (response.isSuccessful) {
+				const message: string = "Deck successfully saved";
+				// Show confirmation modal
+			} else if (response.error) {
+				const error: string = response.error;
+				// Show error modal
+			}
+		});
+	};
+
+	const handleManage = () => {
+		// Open the decks manager modal
+	};
+
+	return (
+		<div id="deck-building-page" className="p-4 overflow-hidden d-flex flex-column h-100 fade-in">
+			<Link to="/lobby" className="arrow"></Link>
+			<div className="blue-container m-4 fade-in p-5 flex-fill d-flex flex-column">
+				<Row>
+					<Col>
+						<div id="deck-stats">
+							{/* CardList && BarChart */}
+							<p>Stats</p>
+							<p>And</p>
+							<p>All</p>
+						</div>
+					</Col>
+				</Row>
+				<Row className="flex-fill">
+					<Col md={10}>
+						<Tabs>
+							<Tab name="Cards">
+								<p>Cards content</p>
+							</Tab>
+							<Tab name="Class">
+								<p>Class content</p>
+							</Tab>
+							<Tab name="Power">
+								<p>Power content</p>
+							</Tab>
+							<Tab name="Faction">
+								<p>Faction content</p>
+							</Tab>
+						</Tabs>
+					</Col>
+					<Col md={2} className="d-flex flex-column justify-content-between">
+						<div id="deck-infos" className="blue-container w-100 d-flex flex-column align-items-center">
+							<p className="w-100 d-flex justify-content-between">
+								<span>Deck Name:</span>
+								<strong>{currentDeck.name ?? "New Deck"}</strong>
+							</p>
+							<Avatar playerClassName={currentDeck.hero?.name} />
+							<p className="w-100 mt-2 d-flex justify-content-between">
+								<span>Selected Talent:</span>
+								<strong>{currentDeck.talent?.name ?? "None"}</strong>
+							</p>
+							<p className="w-100 d-flex justify-content-between">
+								<span>Selected Talent:</span>
+								<strong>{currentDeck.talent?.name ?? "None"}</strong>
+							</p>
+							<p className="w-100 d-flex justify-content-between">
+								<span>Selected Faction:</span>
+								<strong>{currentDeck.faction?.name ?? "None"}</strong>
+							</p>
+						</div>
+						<div id="deck-building-buttons-wrapper">
+							<button className="custom-btn custom-btn-big w-100" onClick={currentDeck.reset}>
+								Reset Deck
+							</button>
+							<button
+								className="custom-btn submit-btn custom-btn-big w-100"
+								onClick={(e) => {
+									handleSave(e.target);
+								}}
+							>
+								Save Deck
+							</button>
+							<button className="custom-btn custom-btn-big w-100" onClick={handleManage}>
+								Manage Decks
+							</button>
+						</div>
+					</Col>
+				</Row>
+			</div>
+		</div>
+	);
 };
 
 export default DeckPage;
--- frontend/src/types/Deck.ts
+++ frontend/src/types/Deck.ts
@@ -1,4 +1,139 @@
+import RequestHandler from "../utils/requestHandler";
+import Card from "./Card";
+import Faction from "./Faction";
+import Hero from "./Hero";
+import Talent from "./Talent";
+
 type Deck = {
-	id: string;
+	id?: string;
 	name: string;
+	hero: Hero;
+	talent: Talent;
+	faction: Faction;
+	active: boolean;
+	cards: Card[];
 };
+export default Deck;
+
+type CreateDeckDto = {
+	id?: string;
+	name: string;
+	heroId: number;
+	talentId: number;
+	factionId: number;
+	cards: number[];
+};
+
+export type DeckOperationResult = {
+	isSuccessful: boolean;
+	error?: string;
+	data?: any;
+};
+
+export class DeckBuilder {
+	id?: string;
+	name?: string;
+	hero?: Hero;
+	talent?: Talent;
+	faction?: Faction;
+	cards: Record<number, number>;
+	cardNumber: number;
+
+	constructor(deck?: Deck) {
+		this.cards = {};
+		this.cardNumber = 0;
+		if (deck) {
+			this.id = deck.id;
+			this.name = deck.name;
+			this.hero = deck.hero;
+			this.talent = deck.talent;
+			this.faction = deck.faction;
+			deck.cards.forEach((card) => this.addCard(card));
+		}
+		this.name = "New Deck";
+	}
+
+	canAddCard = (card: Card): boolean => {
+		return this.cardNumber !== 30 && this.cards[card.id] < 3;
+	};
+
+	addCard = (card: Card): DeckOperationResult => {
+		const result: DeckOperationResult = { isSuccessful: true };
+		if (this.cards[card.id]) {
+			if (this.cards[card.id] < 3) {
+				this.cards[card.id] += 1;
+			} else {
+				result.error = `Cannot add more than 3 of the same card: ${card.id}`;
+				result.isSuccessful = false;
+				return result;
+			}
+		} else {
+			this.cards[card.id] = 1;
+		}
+		this.cardNumber += 1;
+		return result;
+	};
+
+	removeCard = (card: Card): DeckOperationResult => {
+		const result: DeckOperationResult = { isSuccessful: true };
+		if (this.cards[card.id]) {
+			this.cards[card.id] -= 1;
+			this.cardNumber -= 1;
+			if (this.cards[card.id] <= 0) {
+				delete this.cards[card.id];
+			}
+		} else {
+			result.error = "Card not found";
+			result.isSuccessful = false;
+		}
+		return result;
+	};
+
+	validate = (): DeckOperationResult => {
+		const result: DeckOperationResult = { isSuccessful: true };
+		if (this.cardNumber !== 30) {
+			result.error = "Deck needs 30 cards";
+			result.isSuccessful = false;
+			return result;
+		}
+		for (const property of Object.keys(this)) {
+			if (this[property as keyof DeckBuilder] === undefined) {
+				result.error = `${property} can't be empty`;
+				result.isSuccessful = false;
+				break;
+			}
+		}
+		return result;
+	};
+
+	save = async (): Promise<DeckOperationResult> => {
+		const result: DeckOperationResult = this.validate();
+		if (result.isSuccessful) {
+			const body: CreateDeckDto = {
+				name: this.name!,
+				heroId: this.hero!.id,
+				talentId: this.talent!.id,
+				factionId: this.faction!.id,
+				cards: Object.entries(this.cards).flatMap(([id, quantity]) => Array(quantity).fill(Number(id))),
+			};
+			// const response = this.id
+			// 	? await RequestHandler.put<Deck>("deck", {
+			// 			id: this.id,
+			// 			...body,
+			// 	  })
+			// 	: await RequestHandler.post<Deck>("deck", body);
+			// result.isSuccessful = response.success;
+			// result.data = response.data;
+			// result.error = response.message;
+		}
+		return result;
+	};
+
+	reset = (): void => {
+		this.cards = {};
+		this.hero = undefined;
+		this.faction = undefined;
+		this.talent = undefined;
+		this.cardNumber = 0;
+	};
+}
