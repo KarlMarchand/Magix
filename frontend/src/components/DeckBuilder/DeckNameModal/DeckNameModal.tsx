6fcdab0007351a896b098d1d42e3701dbffeaa4c
Karlipouette <karl-marchand@hotmail.com>
2023-12-05 14:31:58 -0500

    Added Deck page and naming modal

---
+++ frontend/src/components/DeckBuilder/DeckNameModal/DeckNameModal.tsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { Modal, ModalProps, Form } from "react-bootstrap";
+import { useDeckManager } from "../../../context/DeckManagerContext/DeckManagerContext";
+import "./deckNameModal.scss";
+
+const DeckNameModal: React.FC<ModalProps> = (props) => {
+	const { currentDeck, changeCurrentDeckName } = useDeckManager();
+	const [name, setName] = useState<string>(currentDeck.name);
+	const [error, setError] = useState<string | null>(null);
+
+	const handleSave = () => {
+		const changeResult = changeCurrentDeckName(name);
+		if (!changeResult.isSuccessful) {
+			setError(changeResult.error ?? "Name can't be changed");
+		} else {
+			props.onHide?.();
+		}
+	};
+
+	return (
+		<Modal {...props} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+			<Modal.Header closeButton>
+				<Modal.Title id="contained-modal-title-vcenter">Edit Deck Name</Modal.Title>
+			</Modal.Header>
+			<Modal.Body>
+				<Form>
+					<Form.Group controlId="deckName">
+						<Form.Label>Deck Name</Form.Label>
+						<Form.Control
+							type="text"
+							placeholder="Enter the new deck name..."
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+							isInvalid={!!error}
+							className="custom-input"
+						/>
+						<Form.Control.Feedback type="invalid">{error}</Form.Control.Feedback>
+					</Form.Group>
+				</Form>
+			</Modal.Body>
+			<Modal.Footer>
+				<button onClick={handleSave} className="custom-btn">
+					Save Changes
+				</button>
+			</Modal.Footer>
+		</Modal>
+	);
+};
+
+export default DeckNameModal;
--- frontend/src/pages/deckPage.tsx
+++ frontend/src/pages/deckPage.tsx
@@ -1,132 +1,113 @@
 import React, { useEffect, useState } from "react";
-import RequestHandler from "../utils/requestHandler";
-import AvailableDeckOptions from "../types/AvailableDeckOptions";
-import { Link } from "react-router-dom";
 import { Col, Row } from "react-bootstrap";
+import { FaRegEdit } from "react-icons/fa";
 import { Tabs, Tab } from "../components/Tabs/Tabs";
-import Faction from "../types/Faction";
-import Hero from "../types/Hero";
-import Talent from "../types/Talent";
-import Card from "../types/Card";
-import Deck, { DeckBuilder } from "../types/Deck";
+import BackButton from "../components/BackButton/BackButton";
 import Avatar from "../components/Avatar/Avatar";
+import "../sass/deckStyle.scss";
+import DeckNameModal from "../components/DeckBuilder/DeckNameModal/DeckNameModal";
+import { useDeckBuilder } from "../context/DeckManagerContext/DeckManagerContext";
 
 const DeckPage: React.FC = () => {
-	const [cardList, setCardList] = useState<Card[]>();
-	const [heroList, setHeroList] = useState<Hero[]>();
-	const [talentList, setTalentList] = useState<Talent[]>();
-	const [factionList, setFactionList] = useState<Faction[]>();
-	const [deckList, setDeckList] = useState<Deck[]>();
-	const [currentDeck, setCurrentDeck] = useState<DeckBuilder>(new DeckBuilder());
+	const { deckBuilder } = useDeckBuilder();
+	const [showDeckNameModal, setShowDeckNameModal] = useState(false);
 
 	useEffect(() => {
-		// Show place holders
-		// Open deck management modal
-		RequestHandler.get<Deck[]>("deck/all").then((response) => {
-			if (response.success && response.data) {
-				setDeckList(response.data);
-			}
-		});
-
-		RequestHandler.get<AvailableDeckOptions>("deck/options/all").then((response) => {
-			if (response.success && response.data) {
-				setCardList(response.data.cards);
-				setHeroList(response.data.heroes);
-				setTalentList(response.data.talents);
-				setFactionList(response.data.factions);
-			}
-			// Remove place holders
-		});
+		// Open deck selection modal
 	}, []);
 
-	const handleSave = (button: EventTarget) => {
-		// Show spinner and disable buttons
-		currentDeck.save().then((response) => {
-			// Remove spinner and enable buttons
+	const handleSave = (button: HTMLButtonElement): void => {
+		// Show spinner
+		deckBuilder.save().then((response) => {
+			// Remove spinner
+			let cssClass = "failed";
+
 			if (response.isSuccessful) {
-				const message: string = "Deck successfully saved";
-				// Show confirmation modal
-			} else if (response.error) {
-				const error: string = response.error;
-				// Show error modal
+				cssClass = "success";
 			}
+
+			button.classList.add(cssClass);
+			setTimeout(() => {
+				button.classList.remove(cssClass);
+			}, 2000);
 		});
 	};
 
-	const handleManage = () => {
-		// Open the decks manager modal
+	const handleChangeDeck = (): void => {
+		alert("Change deck");
 	};
 
 	return (
 		<div id="deck-building-page" className="p-4 overflow-hidden d-flex flex-column h-100 fade-in">
-			<Link to="/lobby" className="arrow"></Link>
+			<BackButton />
 			<div className="blue-container m-4 fade-in p-5 flex-fill d-flex flex-column">
-				<Row>
-					<Col>
-						<div id="deck-stats">
-							{/* CardList && BarChart */}
-							<p>Stats</p>
-							<p>And</p>
-							<p>All</p>
+				<Row className="h-100">
+					<Col xs={10}>
+						<div className="d-flex flex-column h-100">
+							<Row>
+								<Col xs={12} md={2}>
+									<button className="custom-btn w-100" onClick={handleChangeDeck}>
+										Change Deck
+									</button>
+								</Col>
+								<Col xs={12} md={7} className="d-flex justify-content-center align-items-center">
+									<h1 className="me-5 text-truncate">{deckBuilder.name}</h1>
+								</Col>
+								<Col xs={12} md={3} className="container">
+									<Row>
+										<Col xs={4} className="d-flex align-items-center justify-content-end">
+											<FaRegEdit
+												size="40"
+												className="edit-btn me-2"
+												style={{ marginTop: "-0.5rem" }}
+												onClick={() => setShowDeckNameModal(true)}
+												title="Edit Deck Name"
+											/>
+										</Col>
+										<Col xs={8}>
+											<button
+												className="custom-btn submit-btn w-100"
+												onClick={(e) => {
+													handleSave(e.currentTarget);
+												}}
+											>
+												<span>Save Deck</span>
+											</button>
+										</Col>
+									</Row>
+								</Col>
+							</Row>
+							<Row className="flex-grow-1">
+								<Tabs>
+									<Tab name="Cards">
+										<p>Cards content</p>
+									</Tab>
+									<Tab name="Class">
+										<p>Class content</p>
+									</Tab>
+									<Tab name="Power">
+										<p>Power content</p>
+									</Tab>
+									<Tab name="Faction">
+										<p>Faction content</p>
+									</Tab>
+								</Tabs>
+							</Row>
 						</div>
 					</Col>
-				</Row>
-				<Row className="flex-fill">
-					<Col md={10}>
-						<Tabs>
-							<Tab name="Cards">
-								<p>Cards content</p>
-							</Tab>
-							<Tab name="Class">
-								<p>Class content</p>
-							</Tab>
-							<Tab name="Power">
-								<p>Power content</p>
-							</Tab>
-							<Tab name="Faction">
-								<p>Faction content</p>
-							</Tab>
-						</Tabs>
-					</Col>
-					<Col md={2} className="d-flex flex-column justify-content-between">
+					<Col xs={2} className="d-flex flex-column justify-content-between">
+						{/* CardList && BarChart */}
 						<div id="deck-infos" className="blue-container w-100 d-flex flex-column align-items-center">
-							<p className="w-100 d-flex justify-content-between">
-								<span>Deck Name:</span>
-								<strong>{currentDeck.name ?? "New Deck"}</strong>
-							</p>
-							<Avatar playerClassName={currentDeck.hero?.name} />
-							<p className="w-100 mt-2 d-flex justify-content-between">
-								<span>Selected Talent:</span>
-								<strong>{currentDeck.talent?.name ?? "None"}</strong>
-							</p>
-							<p className="w-100 d-flex justify-content-between">
-								<span>Selected Talent:</span>
-								<strong>{currentDeck.talent?.name ?? "None"}</strong>
-							</p>
-							<p className="w-100 d-flex justify-content-between">
-								<span>Selected Faction:</span>
-								<strong>{currentDeck.faction?.name ?? "None"}</strong>
-							</p>
+							<Avatar playerClassName={deckBuilder.hero?.name} />
 						</div>
-						<div id="deck-building-buttons-wrapper">
-							<button className="custom-btn custom-btn-big w-100" onClick={currentDeck.reset}>
-								Reset Deck
-							</button>
-							<button
-								className="custom-btn submit-btn custom-btn-big w-100"
-								onClick={(e) => {
-									handleSave(e.target);
-								}}
-							>
-								Save Deck
-							</button>
-							<button className="custom-btn custom-btn-big w-100" onClick={handleManage}>
-								Manage Decks
-							</button>
-						</div>
+						<div id="deck-building-buttons-wrapper"></div>
+						<button className="custom-btn" onClick={deckBuilder.reset}>
+							Reset Deck
+						</button>
 					</Col>
 				</Row>
 			</div>
+			<DeckNameModal show={showDeckNameModal} onHide={() => setShowDeckNameModal(false)} />
 		</div>
 	);
 };
