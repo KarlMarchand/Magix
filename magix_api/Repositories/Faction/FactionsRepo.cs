c944fd0a54a2878d4098a33b68fe5aed25d0a652
Karlipouette <karl-marchand@hotmail.com>
2023-12-06 19:56:28 -0500

    bug array crashes key

--- magix_api/Repositories/Faction/FactionsRepo.cs
+++ magix_api/Repositories/Faction/FactionsRepo.cs
@@ -14,11 +14,11 @@ namespace magix_api.Repositories
         {
             _context = context;
             _memoryCache = memoryCache;
-            var empireFaction = new Faction { Name = Faction.EMPIRE, Description = "Use the imperial war machine to crush your ennemy with expensive but powerful units." };
-            var rebelFaction = new Faction { Name = Faction.REBEL, Description = "Rebellion are built on hope... and stealth. Experts in deception and ambush to strike when the enemy is off-guard." };
-            var republicFaction = new Faction { Name = Faction.REPUBLIC, Description = "Get behind the legendary Jedi and their mystical powers and beat those clankers." };
-            var separatistFaction = new Faction { Name = Faction.SEPARATIST, Description = "Cheap troops and sheer numbers will win this war. They can't beat us all, we are legion." };
-            var criminalFaction = new Faction { Name = Faction.CRIMINAL, Description = "When you need something done right, you hire the best of the best. With specialised troops and shady characters, the real power lies in the shadow." };
+            var empireFaction = new Faction { Id = 1, Name = Faction.EMPIRE, Description = "Use the imperial war machine to crush your ennemy with expensive but powerful units." };
+            var rebelFaction = new Faction { Id = 2, Name = Faction.REBEL, Description = "Rebellion are built on hope... and stealth. Experts in deception and ambush to strike when the enemy is off-guard." };
+            var republicFaction = new Faction { Id = 3, Name = Faction.REPUBLIC, Description = "Get behind the legendary Jedi and their mystical powers and beat those clankers." };
+            var separatistFaction = new Faction { Id = 4, Name = Faction.SEPARATIST, Description = "Cheap troops and sheer numbers will win this war. They can't beat us all, we are legion." };
+            var criminalFaction = new Faction { Id = 5, Name = Faction.CRIMINAL, Description = "When you need something done right, you hire the best of the best. With specialised troops and shady characters, the real power lies in the shadow." };
             _customFactions = BuildFactionsDictionnary(new List<Faction> { empireFaction, rebelFaction, republicFaction, separatistFaction, criminalFaction });
         }
 
